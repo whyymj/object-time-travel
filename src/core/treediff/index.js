32c348c8279aefdb21fef6f8ec9619ce6d4b7988
武浩 <wuhao1@haier.com>
2021-07-27 16:10:35 +0800

    基本完成正向还原

--- src/core/treediff/index.js
+++ src/core/treediff/index.js
@@ -19,11 +19,21 @@ function isPrimitive(value) {
     )
 }
 
-function getDataType(data) {
-    if (Immutable.isImmutable(data)) {
+function getDataType(data,toJS=false) {
+    if (Immutable.isImmutable(data)&&!toJS) {
         return 'Immutable ' + data.toString().split(' ')[0]
     }
+    if (Immutable.isImmutable(data)&&toJS) {
+        data=data.toJS()
+    }
     return Object.prototype.toString.call(data).split(' ')[1].replace(']', '')
+}
+
+function isNotInThePath(parents, key, floor) {
+    if (parents && parents.get(floor) !== undefined && parents.get(floor) != key) {
+        return true
+    }
+    return false
 }
 /**
  * 找到两个相同结构的对象的差异内容，并返回包含所有差异的数组
@@ -32,58 +42,71 @@ function getDataType(data) {
  * @param obj2 可能修改过的对象
  * @returns {{}} 包含所有差异的数组
  */
-function findObjOperate(obj1, obj2, path = [], resultObj = []) {
+function findObjOperate(obj1, obj2, path, type, resultObj = [], parents) {
 
     if (getDataType(obj1) != getDataType(obj2)) {
-
         resultObj.push({
-            path: [...path],
+            path,
+            type:type.push(getDataType(obj1,true)),
             operation: 'update',
-            from: Immutable.fromJS(obj1),
-            to: Immutable.fromJS(obj2),
+            value: {
+                from: Immutable.fromJS(obj1),
+                to: Immutable.fromJS(obj2),
+            }
         });
     } else {
         if (isPrimitive(obj1) || isPrimitive(obj2)) {
             if (obj1 !== obj2) {
                 resultObj.push({
-                    path: [...path],
+                    path,
+                    type:type.push(getDataType(obj1,true)),
                     operation: 'update',
-                    from: Immutable.fromJS(obj1),
-                    to: Immutable.fromJS(obj2),
+                    value: {
+                        from: Immutable.fromJS(obj1),
+                        to: Immutable.fromJS(obj2),
+                    }
                 });
             }
         } else {
-
             obj1 = Immutable.fromJS(obj1)
             obj2 = Immutable.fromJS(obj2)
-
             const filteKeys2 = {};
             obj2.map((val, key) => {
                 filteKeys2[key] = key
             })
             //old有但new可能没有或者不同
             obj1.map((val, key) => {
+
                 delete filteKeys2[key];
                 key = key + '';
                 if (!Immutable.is(obj1.get(key), obj2.get(key))) {
+                    if (isNotInThePath(parents, key, path.size)) {
+                        return
+                    }
                     //将变化过的属性挂载到返回对象中
                     if (obj2.get(key) !== undefined) {
                         if (typeof obj1.get(key) == 'object' && typeof obj2.get(key) == 'object') {
-                            findObjOperate(obj1.get(key), obj2.get(key), [...path, key], resultObj)
+                            findObjOperate(obj1.get(key), obj2.get(key), path.push(key), type.push(getDataType(obj1,true)), resultObj, parents)
                         } else {
                             resultObj.push({
-                                path: [...path, key],
+                                path: path.push(key),
                                 operation: 'update',
-                                from: obj1.get(key),
-                                to: obj2.get(key),
+                                type:type.push(getDataType(obj1,true)),
+                                value: {
+                                    from: obj1.get(key),
+                                    to: obj2.get(key),
+                                }
                             });
                         }
-
                     } else {
                         resultObj.push({
-                            path: [...path, key],
+                            path: path.push(key),
                             operation: 'delete',
-                            from: obj1.get(key),
+                            type:type.push(getDataType(obj1,true)),
+                            value: {
+                                from: obj1.get(key),
+                                to: undefined,
+                            }
                         });
                     }
                 }
@@ -92,12 +115,19 @@ function findObjOperate(obj1, obj2, path = [], resultObj = []) {
             //new有单old没有
             keys2.forEach(key => {
                 key = key + '';
+                if (isNotInThePath(parents, key, path.size)) {
+                    return
+                }
                 if (obj1.get(key) !== obj2.get(key)) {
                     //将变化过的属性挂载到返回对象中
                     resultObj.push({
-                        path: [...path, key],
-                        to: obj2.get(key),
+                        path: path.push(key),
                         operation: 'add',
+                        type:type.push(getDataType(obj1,true)),
+                        value: {
+                            from: undefined,
+                            to: obj2.get(key),
+                        }
                     })
 
                 }
@@ -114,25 +144,50 @@ function findObjOperate(obj1, obj2, path = [], resultObj = []) {
  * @param list1 原数组
  * @param list2 可能修改过的数组
  */
-function findOperate(olddata, newdata) {
+function findOperate(olddata, newdata, path = []) {
 
     //比较obj1和obj2的差异
-    const objOperateObj = findObjOperate(olddata, newdata);
-    //如果返回的对象为空对象，表示没有差异
-    if (!isEmptyObj(objOperateObj)) {
-        console.log('update', obj1.id, objOperateObj);
-    }
-    return objOperateObj;
+    const objOperateObj = findObjOperate(olddata, newdata, Immutable.List([]), Immutable.List([]), [], (path.length ? Immutable.List(path) : null));
+
+    return Immutable.fromJS(objOperateObj).toJS();
 }
-exports.forward = function (data, operations) {
+exports.transfer = function (data, operations) {
+
     if (typeof data == 'object') {
         operations.forEach(oper => {
+            let path = oper.path;
+            let type=oper.type;
+            if (Immutable.isList(path)) {
+                path = path.toJS();
+            }
+            if (Immutable.isList(type)) {
+                type = type.toJS();
+            }
+            let child = data;
+            for (let i = 0; i < path.length - 1; i++) {
+                if (typeof child[path[i]] != 'object') {
+                    child[path[i]] = {}
+                    if(type[i+1]=='Array'){
+                        child[path[i]]=[]
+                    }
+                }
+                child = child[path[i]];
+            }
+            if (oper.operation == 'add') {
+                child[path[path.length - 1]] = Immutable.isCollection(oper.value.to) ? oper.value.to.toJS() : oper.value.to;
 
+            } else if (oper.operation == 'update') {
+                child[path[path.length - 1]] = Immutable.isCollection(oper.value.to) ? oper.value.to.toJS() : oper.value.to;
+
+            } else if (oper.operation == 'delete') {
+                delete child[path[path.length - 1]];
+
+            }
         })
     } else {
         throw new Error('请输入Object')
     }
+    return data
 
 }
-exports.findObjOperate = findObjOperate;
 exports.findOperate = findOperate
--- test.js
+++ test.js
@@ -1,26 +1,38 @@
- const findObjOperate = require('./src/core/treediff').findObjOperate
+ const findObjOperate = require('./src/core/treediff').findOperate
+ const transfer = require('./src/core/treediff').transfer
  const deepIs = require('deep-is')
- let res = findObjOperate({
+ let res = findObjOperate([{
      name: 'test',
      children: {
          id: 'child1',
-         name: 'child2'
+         name: 'child2',
+         children:[{
+             id:'child22',
+             name:'ooo'
+         }]
      },
      array: [1]
- }, {
+ }], [{
      id: 1,
      children: {
          id: 'child1',
-         name: 'child1'
+         name: 'child1',
+         children:[{
+             id:'child21',
+             name:8888
+         }]
      },
      array: [3,2],
      data:{}
- })
+ }])
  console.log(res)
+ console.log(JSON.stringify(transfer([],res)))
  
-// const Immutable = require("immutable");
-// let num = Immutable.fromJS([{
-//     id: {
-//         children: [1, 2]
-//     }
-// }]);
+const Immutable = require("immutable");
+let data={
+    info:{
+        test:1
+    }
+}
+let info = data.info;
+info.test=2
